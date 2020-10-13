70e9207de433833b087435cf34e542d9461a34a7
Anghealy Gomez Florez <gomezanghealy8.1.2017@gmail.com>
2020-10-13 13:48:14 -0500

    Los archivos reusables

---
+++ Footer.jsx
@@ -0,0 +1,26 @@
+    import React from 'react';
+    import '../../styles/InicioRegistrados/Footer.css';
+    import Derechos from '../../images/DerechosReservado.png';
+
+    class Footer extends React.Component{
+        render(){
+            return(        
+        <footer class="footer">
+            <div class="icons-social">
+                <ul class="list-social">
+
+                    <li class="icons-social__item"><a class="icons-social__link text-light mr-2" href="#"><i class="icons-social__i fab fa-facebook"></i></a></li>
+                    <li class="icons-social__item"><a class="icons-social__link text-light mr-2" href="#"><i class="icons-social__i fab fa-instagram"></i></a></li>
+                    <li class="icons-social__item"><a class="icons-social__link text-light mr-2" href="#"><i class="icons-social__i fab fa-twitter"></i></a></li>
+
+                </ul>
+                <div class="container-footer">
+            <p><img src={Derechos} className="Derechos" />Todos los derechos reservados</p>
+        </div>
+            </div>
+        </footer>
+            )
+        }
+    }
+
+    export default Footer;
